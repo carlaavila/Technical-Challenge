8a559f176d5396635163ac91d3ff88cc1a2f8233
carlaavila <carlita-avila96@hotmail.com>
2021-06-10 14:50:59 -0300

    add SDK mercado pago

---
+++ app/Service/MercadoPago/MercadoPagoService.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Service\MercadoPago;
+
+use MercadoPago\SDK;
+
+require __DIR__ .  '../../../vendor/autoload.php';
+
+SDK::setAccessToken('PROD_ACCESS_TOKEN');
+
+
+class MercadoPagoService
+{
+
+}
